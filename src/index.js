8cd95f5da6dcb5495d6471937295e2eb82698766
liwenxuan <liwenxuan@intra.nsfocus.com>
2022-11-09 17:42:50 +0800

    feat | build new react

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>woshitest <span>sasd</span> </h1> , root);
+
