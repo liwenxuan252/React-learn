51c3bb21389e4aa1068cb46b8c077749002af0c9
liwenxuan <liwenxuan@intra.nsfocus.com>
2022-11-15 14:23:14 +0800

    feat | finish jump Ball and BallList

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import  { createRoot }  from 'react-dom/client';
-import StudentList from './components/StudentList';
+import BallList from './components/BallList';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// 获取学生数据
-async function fetchAllStudents(){
-   const stu = await fetch("./test.json").then(resp => resp.json()).then(resp => resp.data);
-   return stu
-}
-
-// 渲染学生列表
-async function render(){
-    const stus = await fetchAllStudents();
-    return root.render(<StudentList stus={stus} />)
-}
-render()
+root.render(<BallList  />)
